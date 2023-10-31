195d4826a713efeff88091e778b6225a97d49f38
Rodolfo HiOk <rodolfo.okino@gmail.com>
2023-10-30 21:03:30 -0300

    feat: add structure of home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Flex } from 'vcc-ui';
+import { Flex, Spacer, Text } from 'vcc-ui';
 import { getCars } from '@/services/get-cars.services';
 import { CarCard } from '@/components/car-card';
 import '../../public/css/styles.css';
@@ -7,10 +7,35 @@ export default async function Home() {
   const cars = await getCars();
 
   return (
-    <Flex extend={{ margin: 32, flexDirection: 'row', gap: 24 }}>
-      {cars.map((car) => (
-        <CarCard key={car.id} car={car} />
-      ))}
+    <Flex
+      extend={{
+        width: '100vw',
+        height: '100vh',
+        paddingTop: 64,
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+      }}
+    >
+      <Text variant="ootah" subStyle="emphasis">
+        Todos os modelos Recharge
+      </Text>
+      <Spacer size={8} />
+
+      <Flex
+        extend={{
+          maxWidth: '80%',
+          width: '100%',
+          margin: '0 auto',
+          flexDirection: 'row',
+          gap: 24,
+          overflow: 'hidden',
+        }}
+      >
+        {cars.map((car) => (
+          <CarCard key={car.id} car={car} />
+        ))}
+      </Flex>
     </Flex>
   );
 }
